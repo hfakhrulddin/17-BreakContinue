6392dd84179e707b9910116949ef63f87fe1719b
Hussen Fakhrulddin <hf556482@gmail.com>
2016-06-07 21:13:33 -0700

    init3

---
+++ BreakContinue/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BreakContinue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] numbers = new string[] { "First", "Second", "Third", "Fourth", "Fifth" };
+            Console.WriteLine("Starting ........");
+            foreach (string number in numbers)
+            {
+                if (number == "Second")
+                    break;
+                else
+                    continue;
+                Console.WriteLine(number);
+            }
+            Console.WriteLine("Done ........");
+            Console.ReadLine();
+        }
+    }
+}
